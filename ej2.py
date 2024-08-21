913fecc6f93cc603f69cdefc63790839ab0a02c5
darkmian939 <140548399+darkmian939@users.noreply.github.com>
2024-08-21 13:10:34 -0500

    Primer algoritmo

---
+++ ej2.py
@@ -0,0 +1,10 @@
+"""1.crear un repositorio
+   2. Crear 3 ramas 
+   A. Elabore un algoritmo que muestre un menu de opciones: 
+      a)jugar, b)calcular, c)salir
+      guardar en una rama
+   B. Elabore una algoritmo que calcule el promedio de las edades de esta ficha:
+       guardar en segunda rama"""
+       
+       
+print(**"MENU:\na)Jugar\nCalcular\nc)Salir")
